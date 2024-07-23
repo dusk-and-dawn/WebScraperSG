2c44781b4fa5eb00e1f84b7640889d902ff12458
pleocode <pleoleet@gmail.com>
2024-07-23 19:58:22 +0200

    Getting al the data

--- page_navigation.py
+++ page_navigation.py
@@ -1,5 +1,3 @@
-# page navigation
-
 import time
 from selenium import webdriver  
 from selenium.webdriver.common.by import By
@@ -8,22 +6,36 @@ from collections import defaultdict
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Scroll to the bottom of the page to ensure all elements are loaded
-def scroll_to_bottom(driver):
-    last_height = driver.execute_script("return document.body.scrollHeight")
-    while True:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        # time.sleep(2)
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break
-        last_height = new_height
+def setup_driver():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    return webdriver.Chrome(options=options)
 
-        # Function to scroll by a specific number of pixels
+def open_website(driver, url):
+    driver.get(url)
+
+# Function to scroll by a specific number of pixels
 def scroll_by_pixels(driver, pixels):
     driver.execute_script(f"window.scrollBy(0, {pixels});")
     time.sleep(1)  # Adjust the sleep time if necessary
-
-# Scroll in increments of 500 pixels until the bottom of the page
+'''
+def smooth_scroll(driver, scroll_increment=500, scroll_pause_time=1):
+    """Scroll down the webpage incrementally and return True if the end is reached."""
     last_height = driver.execute_script("return document.body.scrollHeight")
-    pixels_to_scroll = 500
+    scroll_position = 0
+
+    while True:
+        driver.execute_script(f"window.scrollTo(0, {scroll_position});")
+        time.sleep(scroll_pause_time)
+
+        # Calculate new scroll position and height
+        scroll_position += scroll_increment
+        new_height = driver.execute_script("return document.body.scrollHeight")
+
+        if scroll_position >= new_height:
+            # Scroll position exceeds or reaches the bottom of the page
+            driver.execute_script(f"window.scrollTo(0, {new_height});")
+            return True  # End of content reached
+
+        last_height = new_height
+'''
