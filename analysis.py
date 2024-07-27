3764fea80fe3df5573f255047ec6f52e2364e4dd
osom2187 <dav.koeller@gmail.com>
2024-07-27 14:40:50 +0200

    comparison between top 100 and top 500 implemented for top 20

--- analysis.py
+++ analysis.py
@@ -19,7 +19,7 @@ df['Rank'] = pd.to_numeric(df['Rank'])
 df['Points'] = pd.to_numeric(df['Points'])
 
 #print(df.head())  
-print(df['Country'].head())
+#print(df['Country'].head())
 
 #preparation for grafics
 hm = {}
@@ -35,16 +35,51 @@ countries_ranked = sorted(hm.items(), key=lambda x:x[1])[::-1]
 #print(countries_ranked)
 countries = [i[0] for i in countries_ranked]
 appearances = [i[1] for i in countries_ranked]
-print(countries, appearances)
+#print(countries, appearances)
 
 #creating the grafic
+
 fig, ax = plt.subplots(figsize=(20,10))
-ax.barh(countries, appearances)
+ax.barh(countries[:20], appearances[:20])
 ax.invert_yaxis()
+ax.set_title('countries representation in the WTA rankings top 500',
+             loc='left', )
 ax.xaxis.set_ticks_position('none')
 ax.yaxis.set_ticks_position('none')
-ax.xaxis.set_tick_params(pad=5)
+ax.xaxis.set_tick_params(pad=10)
 ax.yaxis.set_tick_params(pad=10)
+plt.savefig('top20_wta500_countries_ranked_by_appearance')
 plt.show()
 
 
+# Second Chapter countries representation in Top 100 
+hm100 = {}
+
+
+for index, country in enumerate(df['Country']):
+    if index < 101:
+        if country in hm100.keys():
+            hm100[country] += 1
+        else:
+            hm100[country] = 1
+
+countries100_ranked = sorted(hm100.items(), key=lambda x:x[1])[::-1]
+
+countries100 = [i[0] for i in countries100_ranked]
+appearances100 = [i[1] for i in countries100_ranked]
+
+fig100, ax100 = plt.subplots(figsize=(20,10))
+ax100.barh(countries100[:20], appearances100[:20])
+ax100.invert_yaxis() 
+ax100.set_title('countries representation in the WTA rankings top 100',
+             loc='left', )
+plt.savefig('top20_wta100_countries_ranked_by_appearance')
+plt.show()
+
+#print(countries100, appearances100)
+for i in range(20):
+    print(countries[i], countries100[i])
+    if countries[i] not in countries100[:20]:
+        print(f'this country is in the top 20 when looking at top 500 but not if one only looks at the top 100: {countries[i]}')
+    if countries100[i] not in countries[:20]:
+        print(f'this country is in the top 20 when looking at top 100 but not if one only looks at the top 500: {countries100[i]}')
